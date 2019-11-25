664d4d91faf54ef2eeea305c348706a51e946b43
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2019-11-25 10:18:39 +0000

    Schema com alguns erros

--- schema.sql
+++ schema.sql
@@ -24,7 +24,7 @@ create table anomalia(
     lingua char(3) not null,
     ts timestamp not null,
     descricao text not null,
-    tem_anomalia_redacao boolean not null6
+    tem_anomalia_redacao boolean not null,
 
     primary key(id));
 
@@ -40,3 +40,56 @@ create table duplicado(
     foreign key(item1)
         references item(id)
 )
+
+create table utilizador(
+    email varchar(254),
+    password varchar(80) not null, 
+
+    primary key(email));
+
+create table utilizador_certificado(
+    email varchar(254),
+
+    foreign key(email)
+        references utilizador(email));
+
+create table utilizador_regular(
+    email varchar(254),
+
+    foreign key(email)
+        references utilizador(email));
+
+create table incidencia (
+    anomalia_id integer,
+    item_id integer,
+    email varchar(254),
+
+    foreign key(anomalia_id)
+        references anomalia(id),
+    foreign key(item_id)
+        references item(id),
+    foreign key(email)
+        references utilizador(email));
+
+create table proposta_de_correcao(
+    email varchar(254),
+    nro integer,
+    data_hora datetime,
+    texto text,
+
+    primary key(email, nro),
+    foreign key(email)
+        references utilizador(email));
+
+create table correcao(
+    email varchar(254),
+    nro integer,
+    anomalia_id integer,
+
+    primary key(email, nro, anomalia_id),
+    foreign key(email)
+        references utilizador(email)
+    foreign key(anomalia_id)
+        references incidencia(anomalia_id));
+
+
