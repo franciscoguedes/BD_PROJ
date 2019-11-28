d1ce59f8ff8c01c7d59a97b18721f159d72c2072
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2019-11-28 01:31:42 +0000

    de acordo com o bank.sql (lab1), possivelmente com erros(?)

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ create table local_publico(
     longitude numeric(8,6),
     nome varchar(80) not null,
 
-    primary key(latitude, longitude));
+    constraint pk_local_publico primary key(latitude, longitude));
 
 
 create table item(
@@ -13,8 +13,8 @@ create table item(
     latitude numeric(9,6),
     longitude numeric(8,6),
 
-    primary key(id),
-    foreign key(latitude, longitude)
+    constraint pk_item primary key(id),
+    constraint fk_item_local_publico foreign key(latitude, longitude)
         references local_publico(latitude, longitude));
 
 create table anomalia(
@@ -26,49 +26,56 @@ create table anomalia(
     descricao text not null,
     tem_anomalia_redacao boolean not null,
 
-    primary key(id));
+    constraint pk_anomalia primary key(id));
 
 create table anomalia_redacao(
     id integer,
     zona2 box not null,
     lingua2 char(3) not null,
 
-    primary key(id));
+    constraint pk_anomalia_redacao primary key(id));
 
 create table duplicado(
 
-    foreign key(item1)
-        references item(id)
-)
+    constraint fk_duplicado_item foreign key(item1)
+        references item(id),
+    constraint fk_duplicado_item foreign key(item2)
+        references item(id));
 
 create table utilizador(
     email varchar(254),
     password varchar(80) not null, 
 
-    primary key(email));
+    constraint pk_utilizador primary key(email));
+
+
+
+
 
 create table utilizador_certificado(
     email varchar(254),
 
-    foreign key(email)
+    constraint fk_utilizador_certificado_utilizador foreign key(email)
         references utilizador(email));
 
 create table utilizador_regular(
     email varchar(254),
 
-    foreign key(email)
+    constraint fk_utilizador_regular_utilizador foreign key(email)
         references utilizador(email));
 
+
+--anomalia_id ou id??
 create table incidencia (
     anomalia_id integer,
     item_id integer,
     email varchar(254),
 
-    foreign key(anomalia_id)
+    constraint fk_incidencia_anomalia foreign key(anomalia_id)
         references anomalia(id),
-    foreign key(item_id)
+    constraint fk_incidencia_item foreign key(item_id)
         references item(id),
-    foreign key(email)
+    constraint fk_incidencia_utilizador foreign key(email)
         references utilizador(email));
 
 create table proposta_de_correcao(
@@ -77,8 +84,8 @@ create table proposta_de_correcao(
     data_hora datetime,
     texto text,
 
-    primary key(email, nro),
-    foreign key(email)
+    constraint pk_proposta_de_correcao primary key(email, nro),
+    constraint fk_proposta_de_correcao_utilizador foreign key(email)
         references utilizador(email));
 
 create table correcao(
@@ -86,10 +93,10 @@ create table correcao(
     nro integer,
     anomalia_id integer,
 
-    primary key(email, nro, anomalia_id),
-    foreign key(email)
-        references utilizador(email)
-    foreign key(anomalia_id)
+    constraint pk_correcao primary key(email, nro, anomalia_id),
+    constraint fk_correcao_utilizador foreign key(email)
+        references utilizador(email),
+    constraint fk_correcao_incidencia foreign key(anomalia_id)
         references incidencia(anomalia_id));
 
 
