35ef6203176787e6b02816a6cfe31918b6b50497
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2019-11-29 15:33:31 +0000

    queries

--- queries.sql
+++ queries.sql
@@ -1,2 +1,15 @@
-SELECT * 
-FROM 
+
+select nome, count(*)
+
+from local_publico 
+    inner join item
+        on local_publico.latitude = item.latitude and local_publico.longitude = item.longitude
+    inner join incidencia
+        on incidencia.anomalia_id = item.id
+
+group by nome
+havinh count(*) >= all (
+    select count(*)
+    from 
+    group by nome
+)
